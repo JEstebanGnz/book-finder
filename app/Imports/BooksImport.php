5b7793030f73762a65717c6624cda7fecc8c0547
JEstebanGnz <juanes01.gm@outlook.com>
2024-04-08 10:48:26 -0500

    dashes removed from identifier column

--- app/Imports/BooksImport.php
+++ app/Imports/BooksImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Book;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithUpserts;
 
 class BooksImport implements ToModel, WithUpserts, WithHeadingRow
@@ -13,7 +14,7 @@ class BooksImport implements ToModel, WithUpserts, WithHeadingRow
     public function model(array $row)
     {
         return new Book([
-            'identifier' => $row['isbn'],
+            'identifier' => str_replace('-', '', $row['isbn']),
             'title' => $row['titulo'],
             'price' => $row['precio'],
             'author' => $row['autor'],
@@ -24,5 +25,13 @@ class BooksImport implements ToModel, WithUpserts, WithHeadingRow
     public function uniqueBy()
     {
         return "identifier";
+    }
+
+    public function map($row): array
+    {
+        return [
+            'identifier' => str_replace('-', '', $row['isbn']),
+            // Other columns as needed
+        ];
     }
 }
